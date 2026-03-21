44bc5657bb2424e28c47e56935eee15273e72420
Sagun Shrestha <sgn13@Saguns-MacBook-Pro.local>
2026-03-21 19:33:53 +0900

    production environment

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -17,7 +17,7 @@ type ApiResponse<T> = T;
 // const API_URL = "https://track-it-gilt-zeta.vercel.app/api/v1/"
 
 const axiosApi = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL||"http://127.0.0.1:8000/api/v1",
   // baseURL: "https://track-it-gilt-zeta.vercel.app/api/v1/",
 });
 
